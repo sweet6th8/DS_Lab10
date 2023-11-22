e764b50b7024f65ae9ed610d98c30af989009390
sweet6th8 <thilang0608@gmail.com>
2023-11-23 00:21:53 +0700

    lab10

---
+++ lab10_task1_stack/MyLIFO_App.java
@@ -0,0 +1,128 @@
+package lab10_task1_stack;
+
+import java.util.Stack;
+
+public class MyLIFO_App {
+	// This method reserves the given array
+	public static <E> void reserve(E[] array) {
+		Stack<E> stack = new Stack<>();
+
+		for (E e : array) {
+			stack.push(e);
+		}
+		for (int i = 0; i < array.length; i++) {
+			array[i] = stack.pop();
+		}
+
+	}
+
+	// This method checks the correctness of the given input
+	// i.e. ()(())[]{(())} ==> true, ){[]}() ==> false
+	public static boolean isCorrect(String input) {
+		Stack<Character> stack = new Stack<>();
+		for (Character i : input.toCharArray()) {
+			if (i == '(') {
+				stack.push(')');
+			} else if (i == '[') {
+				stack.push(']');
+			} else if (i == '{') {
+				stack.push('}');
+			} else if (stack.isEmpty() || stack.pop() != i) {
+				return false;
+			}
+
+		}
+		return stack.isEmpty();
+	}
+
+	// This method evaluates the value of anexpression
+	// i.e. 51 + (54 *(3+2)) = 321
+	public static int evaluateExpression(String expression) {
+
+		expression.trim();
+		Stack<Integer> operands = new Stack<>();
+		Stack<Character> operators = new Stack<>();
+//		for (Character c : expression.toCharArray()) {
+		for (int i = 0; i < expression.length(); i++) {
+			char c = expression.charAt(i);
+
+			if (Character.isDigit(c)) {
+				String s = c + "";
+				while (i + 1 < expression.length() && Character.isDigit(expression.charAt(i + 1))) {
+					s = s.concat(expression.charAt(++i) + "");
+				}
+				operands.push(Integer.valueOf(s));
+
+			} else if (c == '+' || c == '-' || c == '*' || c == '/') {
+
+				while (!operators.isEmpty() && hasPrecedence(operators.peek()) >= hasPrecedence(c)) {
+					calculate(operands, operators);
+				}
+				operators.push(c);
+			} else if (c == '(') {
+				operators.push(c);
+			} else if (c == ')') {
+				while (!operators.isEmpty() && operators.peek() != '(') {
+					calculate(operands, operators);
+				}
+				operators.pop();
+
+			}
+		}
+		while (!operators.isEmpty())
+
+		{
+			calculate(operands, operators);
+		}
+
+		return operands.pop();
+	}
+
+	private static int hasPrecedence(char c) {
+		if (c == '+' || c == '-') {
+			return 1;
+		} else if (c == '*' || c == '/') {
+			return 2;
+		}
+		return 0;
+	}
+
+	private static void calculate(Stack<Integer> operands, Stack<Character> operators) {
+		int x = operands.pop();
+		int y = operands.pop();
+		int ops = operators.pop();
+		switch (ops) {
+		case '+': {
+			operands.push(x + y);
+			break;
+		}
+		case '-': {
+			operands.push(y - x);
+			break;
+		}
+		case '*': {
+			operands.push(x * y);
+			break;
+		}
+		case '/': {
+			if (x == 0)
+				throw new UnsupportedOperationException("Cannot divide by zero");
+			operands.push(y / x);
+			break;
+		}
+		}
+
+	}
+
+	public static void main(String[] args) {
+//		Integer[] arr = { 1, 2, 3, 4 };
+//		reserve(arr);
+////		System.out.println(Arrays.toString(arr));
+//		String s = "()(())[]{(())}";
+//		String s2 = "){[]}()";
+//		System.out.println(isCorrect(s));
+//		System.out.println(isCorrect(s2));
+		System.out.println(evaluateExpression("51 + (54 *(3+2))"));
+
+	}
+}
---
+++ lab10_task2_queue/MyLIFO_App.java
@@ -0,0 +1,59 @@
+package lab10_task2_queue;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class MyLIFO_App {
+	// method stutter that accepts a queue of integers as a parameter and replaces
+	// every element of the queue with two copies of that element
+	// front [1, 2, 3] back
+	// becomes
+	// front [1, 1, 2, 2, 3, 3] back
+	public static <E> void stutter(Queue<E> input) {
+		Queue<E> q = new LinkedList<E>();
+		while (!input.isEmpty()) {
+			q.add(input.poll());
+		}
+		while (!q.isEmpty()) {
+			input.add(q.peek());
+			input.add(q.poll());
+
+		}
+	}
+	// Method mirror that accepts a queue of strings as a parameter and appends the
+	// queue's contents to itself in reverse order
+	// front [a, b, c] back
+
+	// becomes
+	// front [a, b, c, c, b, a] back
+	public static <E> void mirror(Queue<E> input) {
+		Queue<E> temp = new LinkedList<>();
+		for (E element : input) {
+            temp.add(element);
+        }
+		
+		Stack<E> s = new Stack<>();
+		
+		while (!temp.isEmpty()) {
+			s.push(temp.poll());
+		}
+		
+		while (!s.isEmpty()) {
+			input.add(s.pop());
+
+		}
+	}
+
+	public static void main(String[] args) {
+		Queue<Integer> q = new ArrayDeque<>();
+		q.offer(1);
+		q.offer(2);
+		q.offer(3);
+		stutter(q);
+//		mirror(q);
+		System.out.println(q);
+	}
+}
